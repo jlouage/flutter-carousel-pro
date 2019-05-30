472a69012967c4367746dfe043268a4f89059916
Leonardo Vitor da Silva <xportation@gmail.com>
2019-05-30 20:15:04 -0300

    Add support for Indicators position and color for the increased dot

--- example/main.dart
+++ example/main.dart
@@ -18,18 +18,22 @@ class CarouselPage extends StatelessWidget {
           height: 150.0,
           width: 300.0,
           child: new Carousel(
+            boxFit: BoxFit.cover,
+            autoplay: false,
+            animationCurve: Curves.fastOutSlowIn,
+            animationDuration: Duration(milliseconds: 1000),
+            dotSize: 6.0,
+            dotIncreasedColor: Color(0xFFFF335C),
+            dotBgColor: Colors.transparent,
+            dotPosition: DotPosition.topRight,
+            dotVerticalPadding: 10.0,
+            showIndicator: true,
+            indicatorBgPadding: 7.0,
             images: [
               new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
               new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
               new ExactAssetImage("assets/images/LaunchImage.jpg"),
             ],
-            onImageTap: (imageIndex) {
-              print("Image: $imageIndex");
-            },
-            onImageChange: (previousImageIndex, currentImageIndex) {
-              print("Previous image index: $previousImageIndex");
-              print("Current image index: $currentImageIndex");
-            },
           ),
         ),
       ),
